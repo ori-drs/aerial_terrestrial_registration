5f0955287890761191df937477b4acf29b077b05
Benoit Casseau <benoit@oxfordrobotics.institute>
2023-09-15 17:25:09 +0100

    refactoring

--- src/digiforest_registration/tasks/height_image.py
+++ src/digiforest_registration/tasks/height_image.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numpy.typing import NDArray
+from numpy import float64
 import cv2
 
 
@@ -6,6 +8,71 @@ class HeightImage:
     def __init__(self):
         self.kernel_size = (3, 3)
         self.debug = False
+        self.min_distance_to_ground = 3.0
+        self.image_resolution = 0.1
+
+    def _point_plane_distance(self, a, b, c, d, array: NDArray[float64]) -> float:
+        """
+        Calculate the distance between a point and a plane
+        ax + by + cz + d = 0
+        """
+        normal = np.tile(np.array([a, b, c]), (array.shape[0], 1))
+        d_array = np.tile(d, (array.shape[0], 1))
+
+        # Calculate the distance using the formula
+        dot_product = np.sum(array * normal, axis=1)
+        dot_product = dot_product[:, np.newaxis]  # make it a column vector
+        numerator = np.abs(dot_product + d_array)
+        denominator = np.linalg.norm(np.array([a, b, c]))
+
+        distance = numerator / denominator
+        return distance
+
+    def compute_canopy_image(self, cloud, a, b, c, d) -> np.ndarray:
+        """
+        Compute the canopy image from a point cloud and a plane
+        A canopy image is a 2D float image where the value of each pixel is the height of the canopy at this pixel
+        """
+        points = np.asarray(cloud.to_legacy().points)
+        dist = self._point_plane_distance(a, b, c, d, points)
+        idx = dist > self.min_distance_to_ground
+        idx = idx.flatten()  # make it a row vector
+        canopy_points = points[idx]
+
+        if self.debug:
+            # Display canopy points as a point cloud
+            import open3d as o3d
+
+            print(a, b, c, d)
+            point_cloud = o3d.geometry.PointCloud()
+            point_cloud.points = o3d.utility.Vector3dVector(canopy_points)
+            o3d.visualization.draw_geometries([point_cloud])
+
+        bounding_box = cloud.get_axis_aligned_bounding_box()
+
+        # Get the minimum and maximum points of the bounding box
+        min_bound = bounding_box.min_bound.numpy()
+        max_bound = bounding_box.max_bound.numpy()
+
+        # Create the canopy image
+        height = int(np.ceil((max_bound[1] - min_bound[1]) / self.image_resolution))
+        width = int(np.ceil((max_bound[0] - min_bound[0]) / self.image_resolution))
+        print("height, width", height, width)
+
+        image = np.zeros((height, width, 1), dtype=np.float32)
+
+        # Fill the image
+        dist = self._point_plane_distance(a, b, c, d, canopy_points)
+        max_height = max(dist)
+        index = 0
+        for point in canopy_points:
+            x = int(np.floor((point[0] - min_bound[0]) / self.image_resolution))
+            y = int(np.floor((point[1] - min_bound[1]) / self.image_resolution))
+            v = abs(dist[index][0] / max_height[0])
+            image[y, x] = max(v, image[y, x])
+            index += 1
+
+        return image
 
     def remove_small_objects(self, img):
         # remove small non-black objects from a float image
--- src/digiforest_registration/tasks/horizontal_alignment.py
+++ src/digiforest_registration/tasks/horizontal_alignment.py
@@ -1,7 +1,3 @@
-import numpy as np
-from numpy.typing import NDArray
-from numpy import float64
-
 from digiforest_registration.tasks.height_image import HeightImage, draw_correspondences
 from digiforest_registration.tasks.graph import Graph, CorrespondenceGraph
 
@@ -16,76 +12,14 @@ class HorizontalRegistration:
         self.cloud_ground_plane = cloud_ground_plane
         self.debug = False
 
-        self.min_distance_to_ground = 3.0
-        self.image_resolution = 0.1
-
-    def point_plane_distance(self, a, b, c, d, array: NDArray[float64]) -> float:
-        """
-        Calculate the distance between a point and a plane
-        ax + by + cz + d = 0
-        """
-        normal = np.tile(np.array([a, b, c]), (array.shape[0], 1))
-        d_array = np.tile(d, (array.shape[0], 1))
-
-        # Calculate the distance using the formula
-        dot_product = np.sum(array * normal, axis=1)
-        dot_product = dot_product[:, np.newaxis]  # make it a column vector
-        numerator = np.abs(dot_product + d_array)
-        denominator = np.linalg.norm(np.array([a, b, c]))
-
-        distance = numerator / denominator
-        return distance
-
-    def compute_canopy_image(self, cloud, a, b, c, d):
-        points = np.asarray(cloud.to_legacy().points)
-        dist = self.point_plane_distance(a, b, c, d, points)
-        idx = dist > self.min_distance_to_ground
-        idx = idx.flatten()  # make it a row vector
-        canopy_points = points[idx]
-
-        if self.debug:
-            # Display canopy points as a point cloud
-            import open3d as o3d
-
-            print(a, b, c, d)
-            point_cloud = o3d.geometry.PointCloud()
-            point_cloud.points = o3d.utility.Vector3dVector(canopy_points)
-            o3d.visualization.draw_geometries([point_cloud])
-
-        bounding_box = cloud.get_axis_aligned_bounding_box()
-
-        # Get the minimum and maximum points of the bounding box
-        min_bound = bounding_box.min_bound.numpy()
-        max_bound = bounding_box.max_bound.numpy()
-
-        # Create the canopy image
-        height = int(np.ceil((max_bound[1] - min_bound[1]) / self.image_resolution))
-        width = int(np.ceil((max_bound[0] - min_bound[0]) / self.image_resolution))
-        print("height, width", height, width)
-
-        image = np.zeros((height, width, 1), dtype=np.float32)
-
-        # Fill the image
-        dist = self.point_plane_distance(a, b, c, d, canopy_points)
-        max_height = max(dist)
-        index = 0
-        for point in canopy_points:
-            x = int(np.floor((point[0] - min_bound[0]) / self.image_resolution))
-            y = int(np.floor((point[1] - min_bound[1]) / self.image_resolution))
-            v = abs(dist[index][0] / max_height[0])
-            image[y, x] = max(v, image[y, x])
-            index += 1
-
-        return image
-
     def process(self):
-        uav_canopy = self.compute_canopy_image(
+        proc = HeightImage()
+        uav_canopy = proc.compute_canopy_image(
             self.reference_cloud, *self.reference_ground_plane
         )
-        bls_canopy = self.compute_canopy_image(self.cloud, *self.cloud_ground_plane)
+        bls_canopy = proc.compute_canopy_image(self.cloud, *self.cloud_ground_plane)
 
         # find maxima in the heigh image
-        proc = HeightImage()
         bls_height_pts, bls_height_img = proc.find_local_maxima(bls_canopy)
 
         uav_height_pts, uav_height_img = proc.find_local_maxima(uav_canopy)
