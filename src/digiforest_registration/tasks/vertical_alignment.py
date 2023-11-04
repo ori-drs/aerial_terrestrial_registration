fix distance between planes computation

src/digiforest_registration/tasks/vertical_alignment.py
@@ -1,9 +1,11 @@
 import numpy as np
 from digiforest_analysis.tasks import GroundSegmentation
 
+import open3d as o3d
+
 
 class VerticalRegistration:
-    def __init__(self, uav_cloud, bls_cloud, ground_segmentation_method):
+    def __init__(self, uav_cloud, bls_cloud, ground_segmentation_method, debug=False):
         self._max_distance_to_plane = 0.5
         self.ground_segmentation = GroundSegmentation(
             max_distance_to_plane=self._max_distance_to_plane,
@@ -14,7 +16,27 @@ class VerticalRegistration:
         )
         self.uav_cloud = uav_cloud
         self.bls_cloud = bls_cloud
-        self.debug = False
+        self.debug = debug
+
+    def project_point_onto_plane(self, point, plane_normal, plane_constant):
+        """
+        Project a 3D point onto a plane.
+
+        Args:
+            point (numpy.ndarray): The 3D point to be projected, e.g., [x, y, z].
+            plane_normal (numpy.ndarray): The normal vector of the plane, e.g., [a, b, c].
+            plane_constant (float): The constant term 'd' in the plane equation, e.g., d.
+
+        Returns:
+            numpy.ndarray: The projected point on the plane.
+        """
+        # Calculate the signed distance from the point to the plane
+        distance = np.dot(point, plane_normal) + plane_constant
+
+        # Calculate the projection of the point onto the plane
+        projected_point = point - distance * plane_normal
+
+        return projected_point
 
     def process(self):
         ground_uav_cloud, _ = self.ground_segmentation.process(cloud=self.uav_cloud)
@@ -49,17 +71,23 @@ class VerticalRegistration:
             inlier_cloud_uav = ground_uav_cloud.select_by_index(inliers_uav)
             inlier_cloud_uav.paint_uniform_color([1.0, 0.0, 0])
 
-            import open3d as o3d
-
             o3d.visualization.draw_geometries(
-                [inlier_cloud.to_legacy(), inlier_cloud_uav.to_legacy()]
+                [inlier_cloud.to_legacy(), inlier_cloud_uav.to_legacy()],
+                window_name="ground point clouds",
             )
 
         # todo find rotation between the two normal vectors
 
         print([a_r, b_r, c_r, d_r], [a, b, c, d])
         print("dot product of normals: ", np.dot(n_r, n))
-        print("Distance between planes: ", np.abs(d_r / norm_r - d / norm))
-        z_offset = -(d_r / norm_r - d / norm)
-        print("z offset bls cloud to uav cloud: ", z_offset)
+        # print("Distance between planes: ", np.abs(d_r / norm_r - d / norm))
+        # z_offset = -(d_r / norm_r - d / norm)
+        # print("z offset bls cloud to uav cloud: ", z_offset)
+
+        uav_point = ground_uav_cloud.point.positions[inliers_uav[0]].numpy()
+        p_proj = self.project_point_onto_plane(uav_point, n, d)
+        print("Distance between planes", np.linalg.norm(p_proj - uav_point))
+        z_offset = np.sign(-(d_r / norm_r - d / norm)) * np.linalg.norm(
+            p_proj - uav_point
+        )
         return [a_r, b_r, c_r, d_r], [a, b, c, d], z_offset