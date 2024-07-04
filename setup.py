e8172f353ebf66f37fc7d76d60627585fe4dcd03
Benoit Casseau <benoit@oxfordrobotics.institute>
2024-07-04 10:33:56 +0100

    update README

--- setup.py
+++ setup.py
@@ -6,3 +6,17 @@ d = generate_distutils_setup(
 )
 
 setup(**d)
+
+
+# from distutils.core import setup
+
+# setup(
+#     name="digiforest_registration",
+#     version="0.0.0",
+#     author="Benoit Casseau",
+#     author_email="benoit@robots.ox.ac.uk",
+#     packages=["digiforest_registration"],
+#     package_dir={"": "src"},
+#     python_requires=">=3.6",
+#     description="Python tools for DigiForest",
+# )
