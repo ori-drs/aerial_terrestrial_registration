b8e0b5ab5808acc903f64137bd5531c8e19bed8b
Benoit Casseau <benoit@oxfordrobotics.institute>
2025-09-24 16:52:34 +0100

    remove unused file

--- src/digiforest_registration/gui/__init__.py
+++ src/digiforest_registration/gui/__init__.py
@@ -1,6 +1,5 @@
 from .main_window import MainWindow
 from .vtk_pointcloud_viewer import VTKPointCloud
-from .o3d_pointcloud_viewer import Open3DViewer
 from .tile_viewer import ShapeCanvas
 from .log_tree_widget import FileTreeWidget
 from .image_viewer import ImageWidget
